94daa76e26a4b67a6f0cbd161250dcd8d0d99786
Muftawo <hajjmuf@gmail.com>
2023-01-20 12:39:42 +0000

    added message decoder

--- projects/caesar_sipher.rb
+++ projects/caesar_sipher.rb
@@ -26,14 +26,29 @@ class CaesarChiper
         end 
         return ans
     end
-            end 
-    end
 
+
+    def decode(message, shift)
+        ans = ""
+        message.each_char do |c|
+            if /[[:upper:]]/.match(c) 
+                ans+= ((c.ord - "A".ord - shift) % 26 + "A".ord ).chr   
+            elsif /[[:lower:]]/.match(c)
+                ans+= ((c.ord - "a".ord - shift)% 26 + "a".ord ).chr   
+            else 
+                ans+= c
+
+            end 
         end 
         return ans
     end
 
+end
 
-puts caesar_cipher("What a string!", 5)
 
 
+messages = CaesarChiper.new
+
+puts messages.encode("What a string!", 5)
+
+puts messages.decode(messages.encode("What a string!", 5),5)
