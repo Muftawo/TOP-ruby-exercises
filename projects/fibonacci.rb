41c2101b56e070d8602383a153b647c225f9810f
Muftawo <hajjmuf@gmail.com>
2023-03-20 14:07:36 +0000

    fib recurssion

--- projects/fibonacci.rb
+++ projects/fibonacci.rb
@@ -20,3 +20,16 @@ end
 
 
 
+def fibs_rec(n)
+  # base cases 0 and 1
+  return [0] if n == 0
+  return [0, 1] if n == 1
+
+  array = fibs_rec(n - 1)
+  # add the last two to form new elements
+  array << array[-2] + array[-1]
+end
+
+
+p fib_rec(8)
+p fib_iteration(8)
